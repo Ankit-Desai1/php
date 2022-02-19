a6911359c4c18c1b30a19e14606d69f4005ddff4
Ankit-Desai1 <95091504+Ankit-Desai1@users.noreply.github.com>
2022-02-19 15:03:24 +0530

    first

--- helperland/Asset/js/book_service.js
+++ helperland/Asset/js/book_service.js
@@ -1,14 +1,157 @@
 function set_service(evt, level) {
     var i, tabcontent, tablinks;
+    var ch = parseInt(level.charAt(4));
+
+    for (j = ch + 1; j <= 4; j++) {
+        document.getElementById("tab" + j + "btn").disabled = true;
+
+    }
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active");
     }
     document.getElementById(level).style.display = "block";
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+$(document).ready(function() {
+    $("#add_address").click(function() {
+        $('#add_address').hide();
+        $('.address_form').show();
+    })
+    $("#cancel").on("click", function() {
+        $('#add_address').show();
+        $('.address_form').hide();
+
+    })
+});
+
+$(document).ready(function() {
+
+    $("#selected_date").datepicker({
+        dateFormat: "dd/mm/yy",
+        minDate: "+1d"
+    });
+
+    var currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    var tomorrow_date = year + "-" + month + "-" + day;
+    document.querySelector('#booking_date').innerHTML = tomorrow_date;
+
+    $("#selected_date").on("change", function() {
+        var selected = $(this).val();
+        document.querySelector('#booking_date').innerHTML = selected;
+        //alert(selected);
+    });
+})
+
+$(document).ready(function() {
+    var servicetime = $("#servicetime option:selected").val();
+    document.querySelector('#basic_time').innerHTML = servicetime;
+    document.querySelector('#total_hours').innerHTML = servicetime;
+    $("#servicetime").on("change", function() {
+        var basic_time = $("#basic_time").text();
+        basic_time = parseFloat(basic_time);
+        var total_time = $("#total_hours").text();
+        total_time = parseFloat(total_time);
+        var extra_time = parseFloat(total_time - basic_time);
+        var service_time = $("#servicetime option:selected").val();
+        service_time = parseFloat(service_time);
+        var newtotal_time = service_time + extra_time;
+        document.querySelector('#basic_time').innerHTML = service_time;
+        document.querySelector('#total_hours').innerHTML = newtotal_time;
+        var total_time = $("#total_hours").text();
+        total_time = parseFloat(total_time);
+        var discount = $("#discount").text();
+        discount = parseFloat(discount);
+        var price = (service_time * 25) + (extra_time * 30);
+        //alert(price);
+        var totalpayment = price - discount;
+        document.querySelector('#total_price').innerHTML = price;
+        document.querySelector('#total_payment').innerHTML = totalpayment;
+        document.querySelector('#effective_price').innerHTML = totalpayment;
+
+    });
+
+});
+
+
+$(document).ready(function() {
+    var selectedtime = $("#selectedtime option:selected").text();
+    // alert(selectedtime);
+    document.querySelector('#booking_time').innerHTML = selectedtime;
+
+    $("#selectedtime").on("change", function() {
+        var selected_time = $("#selectedtime option:selected").text();;
+        document.querySelector('#booking_time').innerHTML = selected_time;
+        //alert(selected);
+    });
+});
+
+$(document).ready(function() {
+    var selectedbed = $("#selectedbed option:selected").val();
+    //alert(servicetime);
+    document.querySelector('#booking_bed').innerHTML = selectedbed;
+
+    $("#selectedbed").on("change", function() {
+        var selected_bed = $("#selectedbed option:selected").val();;
+        document.querySelector('#booking_bed').innerHTML = selected_bed;
+        //alert(selected);
+    });
+});
+
+$(document).ready(function() {
+    var selectedbath = $("#selectedbath option:selected").val();
+    //alert(selectedtime);
+    document.querySelector('#booking_bath').innerHTML = selectedbath;
+
+    $("#selectedbath").on("change", function() {
+        var selected_bath = $("#selectedbath option:selected").val();;
+        document.querySelector('#booking_bath').innerHTML = selected_bath;
+        //alert(selected);
+    });
+});
+
+
+function extrabtnclick(id, i) {
+
+    if (document.getElementById(id).checked) {
+        document.getElementById(id + "Img").src = ("./Asset/image/" + i + "-green.png");
+        // alert("extra" + i);
+        var extratime = 0;
+
+        document.getElementById("extra" + i).style.display = "block";
+        document.getElementById("extra" + i + 1).style.display = "block";
+        var total_time = parseFloat(document.getElementById("total_hours").innerHTML);
+        document.getElementById("total_hours").innerHTML = total_time + 0.5;
+        var total_price = parseFloat(document.getElementById("total_price").innerHTML);
+        total_price = total_price + 15;
+        document.getElementById("total_price").innerHTML = total_price;
+        var discount = parseFloat(document.getElementById("discount").innerHTML);
+        total_payment = total_price - discount;
+        document.getElementById("total_payment").innerHTML = total_payment;
+        document.getElementById("effective_price").innerHTML = total_payment;
+
+    } else {
+        document.getElementById(id + "Img").src = ("./Asset/image/" + i + ".png");
+        document.getElementById("extra" + i).style.display = "none";
+        document.getElementById("extra" + i + 1).style.display = "none";
+        var total_time = parseFloat(document.getElementById("total_hours").innerHTML);
+        document.getElementById("total_hours").innerHTML = total_time - 0.5;
+        var total_price = parseFloat(document.getElementById("total_price").innerHTML);
+        total_price = total_price - 15;
+        document.getElementById("total_price").innerHTML = total_price;
+        var discount = parseFloat(document.getElementById("discount").innerHTML);
+        total_payment = total_price - discount;
+        document.getElementById("total_payment").innerHTML = total_payment;
+        document.getElementById("effective_price").innerHTML = total_payment;
+    }
+}
--- helperland/Controller/HelperlandController.php
+++ helperland/Controller/HelperlandController.php
@@ -10,7 +10,6 @@ class HelperlandController
     public function HomePage()
     {
         include('./View/homepage.php');
-       
     }
 
     public function login()
@@ -18,40 +17,35 @@ class HelperlandController
         session_start();
         $base_url = 'http://localhost/php/helperland/index.php#loginform';
         $base_url2 = 'http://localhost/php/helperland/?controller=Helperland&function=book_service';
-        
+
         if (isset($_POST)) {
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-                
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
             $user = $this->model->check($email);
-           
-            if($user['Email'] !== $email ){
+
+            if ($user['Email'] !== $email) {
                 $_SESSION['message'] = 'Email is not exist.';
                 header('Location: ' . $base_url);
-            }
-            elseif(!password_verify($password,$user['Password'])){
+            } elseif (!password_verify($password, $user['Password'])) {
                 $_SESSION['msg'] = 'Email & Password not match';
                 header('Location: ' . $base_url);
-                }
-            else{ 
-                $_SESSION['username']=$user['FirstName']; 
+            } else {
+                $_SESSION['username'] = $user['FirstName'];
+                $_SESSION['userid'] = $user['UserId'];
                 if (isset($_POST['remember'])) {
                     setcookie('emailcookie', $email, time() + 86400, '/');
                     setcookie('passwordcookie', $password, time() + 86400, '/');
 
-                    if($user['UserTypeId']==1){
-                        echo'service provider';
-                    }
-                    elseif($user['UserTypeId']==2){
-                        header('Location: ' . $base_url2);   
-                    }
-                    else{
+                    if ($user['UserTypeId'] == 1) {
+                        echo 'service provider';
+                    } elseif ($user['UserTypeId'] == 2) {
+                        header('Location: ' . $base_url2);
+                    } else {
                         //echo"admin";
                     }
                 }
-                
-            
-            }                
+            }
         } else {
             echo 'Error Occured Try Again';
         }
@@ -60,55 +54,235 @@ class HelperlandController
     public function contact_us()
     {
         $base_url = 'http://localhost/php/helperland/?controller=Helperland&function=contact';
-        if(isset($_POST)){
-            $array=[
-            'firstname' => $_POST['firstname'].' ' .$_POST['lastname'] ,
-            'phonenumber' => $_POST['phonenumber'],
-            'email' => $_POST['email'],
-            'subject' => $_POST['subject'],
-            'message' => $_POST['message'],
-            'createdon'=> date('Y-m-d')
-        ];
-        $contact = $this->model->contact('contactus',$array);
-        header('Location: ' . $base_url);
-        }
-        else{
+        if (isset($_POST)) {
+            $array = [
+                'firstname' => $_POST['firstname'] . ' ' . $_POST['lastname'],
+                'phonenumber' => $_POST['phonenumber'],
+                'email' => $_POST['email'],
+                'subject' => $_POST['subject'],
+                'message' => $_POST['message'],
+                'createdon' => date('Y-m-d')
+            ];
+            $contact = $this->model->contact('contactus', $array);
+            header('Location: ' . $base_url);
+        } else {
             header('Location: ' . $base_url);
         }
-        
     }
 
-    public function contact(){
+    public function contact()
+    {
         include('./View/contact.php');
     }
 
-   public function become_a_pro(){
-       include('./View/become-a-pro.php');
-   }
+    public function become_a_pro()
+    {
+        include('./View/become-a-pro.php');
+    }
 
-    public function customer_registration(){
+    public function customer_registration()
+    {
         include('./View/customer_registration.php');
     }
 
-    public function reset(){
-        $id= $_GET['parameter'];
+    public function reset()
+    {
+        $id = $_GET['parameter'];
         include('./View/forgot.php');
     }
 
-    public function book_service(){
+    public function book_service()
+    {
         include('./View/book_service.php');
     }
 
-    public function about(){
+    public function about()
+    {
         include('./View/about.php');
     }
 
-    public function faq(){
+    public function faq()
+    {
         include('./View/FAQ.php');
     }
 
-    public function prices(){
+    public function prices()
+    {
         include('./View/prices.php');
     }
+
+    public function check_postalcode()
+    {
+        if (isset($_POST)) {
+            $postal = $_POST['postal'];
+            $count = $this->model->PostalExists($postal);
+            if ($count > 0) {
+                echo 1;
+            } else {
+                echo 0;
+            }
+        }
+    }
+
+    public function city()
+    {
+        if (isset($_POST)) {
+            $pincode = $_POST['postalcode'];
+            $result = $this->model->Location($pincode);
+            $city = $result[0];
+            $state = $result[1];
+            $return = [$city, $state];
+            echo json_encode($return);
+        }
+    }
+
+
+    public function insert_address()
+    {
+        if (isset($_POST)) {
+            $userid = $_POST['userid'];
+            $streetname = $_POST['streetname'];
+            $housenumber = $_POST['housenumber'];
+            $pincode = $_POST['pincode'];
+            $location = $_POST['location'];
+            $phonenumber = $_POST['phonenumber'];
+            $username = $_POST['username'];
+            $type = 2;
+            $res = $this->model->Get_email($userid);
+            $email = $res[0];
+            $state = $this->model->Location($pincode);
+            $state = $state[1];
+
+            $array = [
+                'userid' => $userid,
+                'streetname' => $streetname,
+                'housenumber' => $housenumber,
+                'location' => $location,
+                'state' => $state,
+                'pincode' => $pincode,
+                'phonenumber' => $phonenumber,
+                'email' => $email,
+                'type' => $type,
+            ];
+            $result = $this->model->Insert_address($array);
+        }
+    }
+
+    public function Get_address()
+    {
+        if (isset($_POST)) {
+            $userid = $_POST['userid'];
+            $result = $this->model->Get_address($userid);
+
+            if (count($result)) {
+                foreach ($result as $row) {
+                    $street = $row['AddressLine1'];
+                    $houseno = $row['AddressLine2'];
+                    $city = $row['City'];
+                    $pincode = $row['PostalCode'];
+                    $mobile = $row['Mobile'];
+                    $isdefault = $row['IsDefault'];
+                    $isdeleted = $row['IsDeleted'];
+                    $addressid = $row['AddressId'];
+                    if ($isdefault == 1) {
+                        $isdefault =  'checked';
+                    } else {
+                        $isdefault = '';
+                    }
+                    if ($isdeleted == 0) {
+                        $output = '<div class="radiobutton">
+                                <input type="radio" id="address' . $addressid . '" name="address" value="' . $addressid . '" class="address-radio" ' . $isdefault . '>     
+                                <label for="address1"><span>ADDRESS:</span><span>' . $street . '  ' . $houseno . ' , ' . $city . ' ' . $pincode . '</span> <br />
+                                <span>PHONE NUMBER:</span> <span>' . $mobile . '</span></label>
+                            
+                        </div>';
+                        echo $output;
+                    }
+                }
+            }
+        } else {
+            echo ('something went wrong');
+        }
+    }
+
+
+    public function Service_request()
+    {
+
+        if (isset($_POST)) {
+            $username = $_POST['username'];
+            $userid  = $_POST['userid'];
+            $servicestartdate = $_POST['servicestartdate'];
+            $zipcode = $_POST['zipcode'];
+            $servicehourrate = $_POST['servicehourrate'];
+            $servicehours = $_POST['servicehours'];
+            $extrahour = $_POST['extrahour'];
+            $subtotal = $_POST['subtotal'];
+            $discount = $_POST['discount'];
+            $totalcost = $_POST['totalcost'];
+            $extraservice = $_POST['extraservice'];
+            $comments = $_POST['comments'];
+            $addressid = $_POST['addressid'];
+            $haspets = $_POST['haspets'];
+            $status = 'Pending';
+            $date = date('Y-m-d H:i:s');
+            $paymentdone = 1;
+            $recordversion = 1;
+
+
+
+            $array = [
+                'userid' => $userid,
+                'servicestartdate' => $servicestartdate,
+                'zipcode'   => $zipcode,
+                'servicehourrate' => $servicehourrate,
+                'servicehours' => $servicehours,
+                'extrahour' => $extrahour,
+                'subtotal' => $subtotal,
+                'discount' => $discount,
+                'totalcost' => $totalcost,
+                'extraservice' => $extraservice,
+                'comments' => $comments,
+                'pets' => $haspets,
+                'status' => $status,
+                'createddate' => $date,
+                'paymentdone' => $paymentdone,
+                'recordversion' => $recordversion,
+            ];
+            $get_address = $this->model->get_selected_address($addressid);
+            $result = $this->model->ADD_Service_request($array);
+
+            if ($result) {
+                $servicerequestid = $result;
+
+
+                foreach ($get_address as $row) {
+                    $street = $row['AddressLine1'];
+                    $houseno = $row['AddressLine2'];
+                    $city = $row['City'];
+                    $pincode = $row['PostalCode'];
+                    $mobile = $row['Mobile'];
+                    $email = $row['Email'];
+                    $state = $row['State'];
+                    $type = $row['Type'];
+                }
+                include('services_booked.php');
+                echo $result;
+                $address = [
+                    'servicerequestid' => $servicerequestid,
+                    'street' => $street,
+                    'houseno' => $houseno,
+                    'city' => $city,
+                    'pincode' => $pincode,
+                    'mobile' => $mobile,
+                    'email' => $email,
+                    'state' => $state,
+                    'type' => $type,
+                ];
+                $service_address = $this->model->service_address($address);
+            } else {
+                echo 0;
+            }
+        }
+    }
 }
-?>
